b72608b0f9ecefeff5f6e9fa9ae29715ca2e7e4f
tayou89 <ysh00718@gmail.com>
2023-07-03 15:18:23 +0900

    23/07/03 15:18

---
+++ philo/philo.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tayou <tayou@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/03 12:45:24 by tayou             #+#    #+#             */
+/*   Updated: 2023/07/03 12:53:37 by tayou            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+typedef struct s_data{
+
+
+#endif
